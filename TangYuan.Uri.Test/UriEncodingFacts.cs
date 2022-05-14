d61263a720560855ce2ca74151669c9bb3f1770e
Liu Xia <lxconan@gmail.com>
2022-05-15 01:09:17 +0800

    feat: Adding escaping feature.

---
+++ TangYuan.Uri.Test/UriEncodingFacts.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TangYuan.Uri.Test;
+
+public class UriEncodingFacts
+{
+    [Fact]
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    public void should_not_encoding_characters_if_they_are_unreserved_characters()
+    {
+        const string unreservedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~";
+        Assert.Equal(unreservedCharacters, UriEncoding.Encode(unreservedCharacters));
+    }
+
+    [Theory]
+    [InlineData("%", "%25")]
+    [InlineData(" <>#%+{}|\\^[]`;/?:@=&$", "%20%3C%3E%23%25%2B%7B%7D%7C%5C%5E%5B%5D%60%3B%2F%3F%3A%40%3D%26%24")]
+    public void should_encoding_characters_which_are_not_unreserved_characters(string data, string expectedEncoded)
+    {
+        Assert.Equal(expectedEncoded, UriEncoding.Encode(data));
+    }
+}
---
+++ TangYuan.Uri/UriCharacters.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace TangYuan.Uri
+{
+    public class UriCharacters
+    {
+        private static readonly HashSet<char> ReservedCharacters = new HashSet<char>
+        {
+            ':', '/', '?', '#', '[', ']', '@', // gen-delims
+            '!', '$', '&', '\'', '(', ')', '*', '+', ',', ';', '=' // sub-delims
+        };
+
+        private static readonly HashSet<char> GeneralDelimiters = new HashSet<char>
+        {
+            ':', '/', '?', '#', '[', ']', '@',
+        };
+        
+        private static readonly HashSet<char> SubDelimiters = new HashSet<char>
+        {
+            '!', '$', '&', '\'', '(', ')', '*', '+', ',', ';', '='
+        };
+
+        public static bool IsReservedCharacters(char c) => ReservedCharacters.Contains(c);
+        public static bool IsGeneralDelimiters(char c) => GeneralDelimiters.Contains(c);
+        public static bool IsSubDelimiters(char c) => SubDelimiters.Contains(c);
+
+        public static bool IsUnreservedCharacter(char c)
+        {
+            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') ||
+                   c == '-' || c == '.' || c == '_' || c == '~';
+        }
+    }
+}
